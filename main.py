3c48440c0425abae457a899a57c9c57c848092ca
Michael Nycz <mnycz04@gmail.com>
2025-10-27 10:35:46 -0400

    implemented euler's method

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 # main.py
 from function import Function
+from methods import *
+
+
+if __name__ == '__main__':
+    exit(0)
--- methods.py
+++ methods.py
@@ -1,18 +1,29 @@
 from function import Function
 import numpy as np
-from decimal import *
+from decimal import Decimal
 
-def eulers(dydt: Function, t0: Decimal, y0: Decimal, tn:Decimal, *, step: Decimal = .1, ):
+def eulers(dydt: Function, t, y, tn, *, step = Decimal(.1) ):
     """
     Approximates a ODE of f(t, y) starting at (t0, y0) and going till tn.
     Uses a default step size of .1 unless step is specified
     :param dydt: ODE function in terms of f(t, y)
-    :param t0: initial time
-    :param y0: initial u
+    :param t: initial time
+    :param y: initial u
     :param tn: point in time to calculate up to
     :param step: step size, with default value of .1
     :return:
     """
+    t, y, tn, step = Decimal(t), Decimal(y), Decimal(tn), Decimal(step)
+    n = 0
+    while t < tn:
+        y = y + dydt(t, y) * ((t + step) - t)
+        t = t + step
+        print(f'Time: {t}')
+        n = n + 1
+
+    print(f'Took {n} steps.')
+    return y
+
     pass
 
 def taylors(dydt: Function):
