c7220ea6b5c1ff1b6eaa745ab4cc65663158201b
Michael Nycz <mnycz04@gmail.com>
2025-10-27 11:33:11 -0400

    implemented taylor's method

--- main.py
+++ main.py
@@ -4,4 +4,10 @@ from methods import *
 
 
 if __name__ == '__main__':
+    f = Function(equation=lambda t, y:  1 - t + (4 * y))
+    dfdy = Function(equation=lambda t, y: 4)
+    dfdt = Function(equation=lambda t, y: -1)
+    print(eulers(f, 0, 1, 1))
+    print(taylors(f, dfdy, dfdt, 0, 1, 1))
+
     exit(0)
--- methods.py
+++ methods.py
@@ -2,7 +2,8 @@ from function import Function
 import numpy as np
 from decimal import Decimal
 
-def eulers(dydt: Function, t, y, tn, *, step = Decimal(.1) ):
+
+def eulers(dydt: Function, t, y, tn, *, step=Decimal(.1)):
     """
     Approximates a ODE of f(t, y) starting at (t0, y0) and going till tn.
     Uses a default step size of .1 unless step is specified
@@ -14,20 +15,32 @@ def eulers(dydt: Function, t, y, tn, *, step = Decimal(.1) ):
     :return:
     """
     t, y, tn, step = Decimal(t), Decimal(y), Decimal(tn), Decimal(step)
-    n = 0
-    while t < tn:
-        y = y + dydt(t, y) * ((t + step) - t)
-        t = t + step
-        print(f'Time: {t}')
-        n = n + 1
 
-    print(f'Took {n} steps.')
+    overflow = 0
+    while t < tn:
+        y += dydt(t, y) * ((t + step) - t)
+        t += step
+
+        overflow += 1
+        if overflow > 2 ** 16:
+            raise(IndexError("Overflow limit exceeded"))
+
     return y
 
     pass
 
-def taylors(dydt: Function):
-    pass
+
+def taylors(f: Function, dfdy: Function, dfdt: Function, t, y, tn, *, step=Decimal(.1)):
+
+    t, y, tn, step = Decimal(t), Decimal(y), Decimal(tn), Decimal(step)
+
+    while( t < tn):
+        y += (step * f(t, y)) + (((step ** 2) / 2) * (dfdy(t, y) * f(t, y) + dfdt(t, y)))
+
+        t += step
+
+    return y
+
 
 def rk4(dydt: Function):
     pass
